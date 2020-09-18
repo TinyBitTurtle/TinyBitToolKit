cd4fc3f857cb49701bfa56bcb917e82b6ed533dc
tinybitturtle@gmail.com <tinybitturtle@gmail.com>
2020-09-17 17:16:57 -0700

    added CameraShakeCtrl to Toolkit Package

--- Runtime/Gems/ActionCtrl.cs
+++ Runtime/Gems/ActionCtrl.cs
@@ -5,7 +5,7 @@ using Object = System.Object;
 
 namespace TinyBitTurtle.Toolkit
 {
-    public partial class  ActionCtrl : SingletonMonoBehaviour<ActionCtrl>
+    public class  ActionCtrl : SingletonMonoBehaviour<ActionCtrl>
     {
         public void DebugOutput(string actionName) { Debug.LogError(CRC32.Compute(actionName) + "/*" + actionName + "*/");}
     }
